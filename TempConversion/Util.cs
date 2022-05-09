6a07689b1eb0c69069da93416cd84ee9b6f47f7d
pranali2808 <ranepranali1997@gmail.com>
2022-05-09 10:56:28 +0530

    Welcome to Temp Conversion Program

---
+++ TempConversion/Util.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempConversion
+{
+    class Util
+    {
+        public static void TempConversion()
+        {
+            int temp;
+            Console.WriteLine("Enter the temp you want to convert") ;
+
+            int temp = Convert.ToInt32();
+            
+           
+
+
+
+        }
+        
+    }
+    
+}
